f11b63d446ee0c6cd8b71c5ea46f2ad073c9c710
RanungPark <ckehfqkr302@naver.com>
2023-06-21 15:05:59 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import InputData from './components/InputData/InputData';
+import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
   return (
     <>
       <InputData />
+      <LoginForm />
     </>
   );
 }
